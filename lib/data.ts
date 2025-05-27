750c6ba36537b66d9ac18be432259c963f96f291
KALYANI <22b01a12a0@svecw.edu.in>
2025-05-27 12:18:42 +0530

    connected to DB and seeded data

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs"
+
 const data = {
+    users:[
+        {
+            name : 'Jhon',
+            email : 'admin@example.com',
+            password:bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name : 'Jane',
+            email : 'user@example.com',
+            password:bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
    products : [
     {
         name:'Silk Saree',
